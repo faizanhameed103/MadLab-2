8f65badf886926c797522492e2f97d9f51b2e042
faizanhameed103 <fhameed103@gmail.com>
2024-03-08 14:01:12 +0500

    Task 1 done

--- Lab2/Task1/app/src/main/java/com/example/lab2t1/MainActivity.kt
+++ Lab2/Task1/app/src/main/java/com/example/lab2t1/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.lab2t1
 
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2t1.ui.theme.Lab2T1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +44,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    var buttonPressed by remember {
+        mutableStateOf(false)
+    }
+
+    val btnColor = if (buttonPressed) Color.Red else Color.Green
+Column (
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+    modifier = Modifier.fillMaxSize().padding(16.dp)
+
+){
+    Button(
+        onClick = {
+            buttonPressed = !buttonPressed
+        },
+        colors = ButtonDefaults.buttonColors(btnColor)
+    ) {
+        Text(text = "Click Me")
+    }
 }
 
-@Preview(showBackground = true)
+}
+
 @Composable
 fun GreetingPreview() {
     Lab2T1Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
